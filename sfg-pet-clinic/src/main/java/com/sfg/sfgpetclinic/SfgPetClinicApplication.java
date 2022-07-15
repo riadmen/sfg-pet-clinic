8e43a5c5829c840995cb3adce118350380510437
riadmen <riad.mennour@owiin.com>
2022-07-15 20:38:39 +0100

    Initial Project Add

---
+++ sfg-pet-clinic/src/main/java/com/sfg/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sfg.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ sfg-pet-clinic/src/test/java/com/sfg/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
